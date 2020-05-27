977729d5c7ce77c59cba447dec6d28fabf0194b2
Tumble <tumblegamer@gmail.com>
2020-05-27 11:27:54 +0100

    file

---
+++ fps.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         FPS
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0.10
+// @description  try to take over the world!
+// @author       TumbleGamer
+// @match        https://play.boxcritters.com/*
+// @match        http://play.boxcritters.com/*
+// @run-at       document-end
+// @grant        unsafeWindow
+// ==/UserScript==
+
+console.info("-----------------------------------");
+console.info("[FPS]");
+console.info("A mod created by TumbleGamer");
+console.info("-----------------------------------");
+
+window = unsafeWindow || window;
+
+function getTime() {
+	//return new Date().getTime()/1000;
+	return performance.now()/1000
+}
+var stage = document.getElementById('stage')
+var counter = document.createElement('p');
+counter.className = "text-right";
+stage.parentElement.appendChild(counter);
+var last = getTime();
+var now,dt,fps;
+now = dt = fps = 0;
+var lastFPS = [];
+var buffer = 10;
+var roundBy = 1;
+
+window.fps = fps;
+
+function update() {
+	now = getTime();
+	dt = now-last;
+	fps = 1/dt;
+	last = now;
+}
+
+window.addEventListener('load', function () {
+	console.log("load")
+createjs.Ticker.on("tick", function(t) {
+	update();
+	lastFPS.unshift(fps);
+	if(lastFPS.length>buffer)lastFPS.length = buffer;
+	var averageFPS = lastFPS.reduce((a, b) => a + b, 0)/lastFPS.length;
+	counter.innerText = "FPS: " + Math.round(averageFPS/roundBy)*roundBy;
+});
+})
