741ba3951a16e19034f6bb9254ab46c5365bd5fb
Tumble <tumblegamer@gmail.com>
2020-10-23 15:01:18 +0100

    namespace

--- fps.user.js
+++ fps.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         FPS
-// @namespace    http://tampermonkey.net/
+// @namespace    https://boxcrittersmods.ga/authors/tumblegamer/
+// @supportURL   http://discord.gg/D2ZpRUW
 // @version      0.1.1.11
 // @description  try to take over the world!
 // @author       TumbleGamer
@@ -14,23 +15,20 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
-console.info("-----------------------------------");
-console.info("[FPS]");
-console.info("A mod created by TumbleGamer");
-console.info("-----------------------------------");
+console.info("[FPS] A mod created by TumbleGamer");
 
 window = unsafeWindow || window;
 
 function getTime() {
 	//return new Date().getTime()/1000;
-	return performance.now()/1000
+	return performance.now() / 1000;
 }
-var stage = document.getElementById('stage')
+var stage = document.getElementById('stage');
 var counter = document.createElement('p');
 counter.className = "text-right";
 stage.parentElement.appendChild(counter);
 var last = getTime();
-var now,dt,fps;
+var now, dt, fps;
 now = dt = fps = 0;
 var lastFPS = [];
 var buffer = 10;
@@ -40,18 +38,18 @@ window.fps = fps;
 
 function update() {
 	now = getTime();
-	dt = now-last;
-	fps = 1/dt;
+	dt = now - last;
+	fps = 1 / dt;
 	last = now;
 }
 
 window.addEventListener('load', function () {
-	console.log("load")
-createjs.Ticker.on("tick", function(t) {
-	update();
-	lastFPS.unshift(fps);
-	if(lastFPS.length>buffer)lastFPS.length = buffer;
-	var averageFPS = lastFPS.reduce((a, b) => a + b, 0)/lastFPS.length;
-	counter.innerText = "FPS: " + Math.round(averageFPS/roundBy)*roundBy;
+	console.log("load");
+	createjs.Ticker.on("tick", function (t) {
+		update();
+		lastFPS.unshift(fps);
+		if (lastFPS.length > buffer) lastFPS.length = buffer;
+		var averageFPS = lastFPS.reduce((a, b) => a + b, 0) / lastFPS.length;
+		counter.innerText = "FPS: " + Math.round(averageFPS / roundBy) * roundBy;
+	});
 });
-})
